ce1cd5d582aaab7a806f653a754f8a4e09618660
Arseniy Rayotov <daxkod3243@gmail.com>
2025-06-01 15:13:54 +0300

    Add files via upload

---
+++ src/hexos-16/kernel/core/handlers.asm
@@ -0,0 +1,81 @@
+; hexos/src/kernel/core/handlers.asm
+
+process_command:
+    pusha
+    mov si, input_buffer
+    
+.skip_spaces:
+    lodsb
+    cmp al, ' '
+    je .skip_spaces
+    dec si
+    
+    cmp byte [si], 0
+    je .done
+    
+    mov di, command_buffer
+.copy_command:
+    lodsb
+    cmp al, ' '
+    je .command_copied
+    cmp al, 0
+    je .command_copied
+    mov [di], al
+    inc di
+    jmp .copy_command
+.command_copied:
+    mov byte [di], 0
+    
+    mov si, command_buffer
+    
+    mov di, clear_cmd
+    call strcmp
+    jc .do_clear
+    
+    mov di, exit_cmd
+    call strcmp
+    jc .do_exit
+    
+    mov di, help_cmd
+    call strcmp
+    jc .do_help
+    
+    mov di, hexfetch_cmd
+    call strcmp
+    jc .do_hexfetch
+    
+    mov di, panic_cmd
+    call strcmp
+    jc .do_panic
+
+    mov si, unknown_cmd
+    call print_string
+    mov si, available_cmds2
+    call print_string
+    jmp .done
+    
+.do_clear:
+    call clear_screen
+    jmp .done
+.do_exit:
+    call do_exit
+    jmp .done
+.do_help:
+    call show_help
+    jmp .done
+.do_hexfetch:
+    call hexfetch
+    jmp .done
+.do_panic:
+    call kernel_panic
+.done:
+    popa
+    ret
+
+welcome_msg db "HexOS v0.3", 0x0D, 0x0A, 0
+available_cmds db "Type 'help' for available commands", 0x0D, 0x0A, 0x0A, 0
+available_cmds2 db "Type 'help' for available commands", 0x0D, 0x0A, 0
+prompt db "# ", 0
+unknown_cmd db "Unknown command...", 0x0D, 0x0A, 0
+input_buffer times 64 db 0
+command_buffer times 16 db 0
---
+++ src/hexos-16/kernel/core/include_dbc.asm
@@ -0,0 +1,7 @@
+; hexos/src/kernel/core/include_dbc.asm
+
+%include "src/kernel/core/dbc/clear.asm"
+%include "src/kernel/core/dbc/exit.asm"
+%include "src/kernel/core/dbc/help.asm"
+%include "src/kernel/core/dbc/hexfetch.asm"
+%include "src/kernel/core/dbc/panic.asm"
