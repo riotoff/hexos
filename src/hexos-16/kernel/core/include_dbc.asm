f90233735b757044f80de3fe8172fe9aa70ff0d6
Arseniy Rayotov <daxkod3243@gmail.com>
2025-06-01 15:20:18 +0300

    Update include_dbc.asm

--- src/hexos-16/kernel/core/include_dbc.asm
+++ src/hexos-16/kernel/core/include_dbc.asm
@@ -1,7 +1,7 @@
-; hexos/src/kernel/core/include_dbc.asm
+; hexos/src/hexos-16/kernel/core/include_dbc.asm
 
-%include "src/kernel/core/dbc/clear.asm"
-%include "src/kernel/core/dbc/exit.asm"
-%include "src/kernel/core/dbc/help.asm"
-%include "src/kernel/core/dbc/hexfetch.asm"
-%include "src/kernel/core/dbc/panic.asm"
+%include "src/hexos-16/kernel/core/dbc/clear.asm"
+%include "src/hexos-16/kernel/core/dbc/exit.asm"
+%include "src/hexos-16/kernel/core/dbc/help.asm"
+%include "src/hexos-16/kernel/core/dbc/hexfetch.asm"
+%include "src/hexos-16/kernel/core/dbc/panic.asm"
