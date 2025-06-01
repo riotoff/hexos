c030903e39693c4b094b544f136538271f0f714d
Arseniy Rayotov <daxkod3243@gmail.com>
2025-06-01 20:09:34 +0300

    Update hexfetch.asm

--- src/hexos-16/kernel/core/dbc/hexfetch.asm
+++ src/hexos-16/kernel/core/dbc/hexfetch.asm
@@ -2,12 +2,12 @@
 
 hexfetch_cmd db "hexfetch", 0
 
-hexfetch_title db "    #   #             hexfetch", 0x0D, 0x0A, 0
-hexfetch_separator db "  ########            --------", 0x0D, 0x0A, 0
-hexfetch_host db "   #   #              Host: ?", 0x0D, 0x0A, 0
-hexfetch_os db " ########             OS: HexOS 16-bit", 0x0D, 0x0A, 0
-hexfetch_cpuu db "  #   #               CPU: ?", 0x0D, 0x0A, 0
-hexfetch_memory db "                      Memory: ?/? KiB", 0x0D, 0x0A, 0
+hexfetch_title db "    #   #     hexfetch", 0x0D, 0x0A, 0
+hexfetch_separator db "  ########    --------", 0x0D, 0x0A, 0
+hexfetch_host db "   #   #      Host: ?", 0x0D, 0x0A, 0
+hexfetch_os db " ########     OS: HexOS 16-bit", 0x0D, 0x0A, 0
+hexfetch_cpuu db "  #   #       CPU: ?", 0x0D, 0x0A, 0
+hexfetch_memory db "              Memory: ?/? KiB", 0x0D, 0x0A, 0
 
 hexfetch:
     pusha
@@ -25,5 +25,3 @@ hexfetch:
     call print_string
     popa
     ret
-
-
