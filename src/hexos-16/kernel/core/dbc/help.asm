2c5c218d26fc0694b9e12b052dd9b13834a9a2f8
Arseniy Rayotov <daxkod3243@gmail.com>
2025-06-02 15:25:45 +0300

    Update help.asm

--- src/hexos-16/kernel/core/dbc/help.asm
+++ src/hexos-16/kernel/core/dbc/help.asm
@@ -7,7 +7,12 @@ help_clear db "  clear - Clear the screen", 0x0D, 0x0A, 0
 help_exit db "  exit  - Shutdown the computer", 0x0D, 0x0A, 0
 help_help db "  help  - Show this help message", 0x0D, 0x0A, 0
 help_hexfetch db "  hexfetch - Show system info", 0x0D, 0x0A, 0
-help_panic db "  panic - Trigger kernel panic (debug)", 0x0D, 0x0A, 0
+help_panic db "  panic - Trigger kernel panic", 0x0D, 0x0A, 0
+help_touch db "  touch - Create new file", 0x0D, 0x0A, 0
+help_ls db "  ls - Look at files list", 0x0D, 0x0A, 0
+help_rm db "  rm - Remove file", 0x0D, 0x0A, 0
+help_echo db "  echo - Echo your words or input it into file", 0x0D, 0x0A, 0
+help_exec db "  exec - View file contents", 0x0D, 0x0A, 0
 
 show_help:
     pusha
@@ -22,6 +27,16 @@ show_help:
     mov si, help_hexfetch
     call print_string
     mov si, help_panic
+    call print_string
+    mov si, help_touch
+    call print_string
+    mov si, help_ls
+    call print_string
+    mov si, help_rm
+    call print_string
+    mov si, help_echo
+    call print_string
+    mov si, help_exec
     call print_string
     popa
     ret
