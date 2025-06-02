2ebc728cc3e3d552b920e6d9980d741a03a888d7
Arseniy Rayotov <daxkod3243@gmail.com>
2025-06-02 15:19:55 +0300

    Update include_dbc.asm

--- src/hexos-16/kernel/core/include_dbc.asm
+++ src/hexos-16/kernel/core/include_dbc.asm
@@ -5,3 +5,8 @@
 %include "src/hexos-16/kernel/core/dbc/help.asm"
 %include "src/hexos-16/kernel/core/dbc/hexfetch.asm"
 %include "src/hexos-16/kernel/core/dbc/panic.asm"
+%include "src/hexos-16/kernel/core/dbc/touch.asm"
+%include "src/hexos-16/kernel/core/dbc/ls.asm"
+%include "src/hexos-16/kernel/core/dbc/rm.asm"
+%include "src/hexos-16/kernel/core/dbc/echo.asm"
+%include "src/hexos-16/kernel/core/dbc/exec.asm"
