5a92cc3daf2fcfdd04ae90b3272f8d2cb3e7abf5
Arseniy Rayotov <daxkod3243@gmail.com>
2025-06-03 11:43:45 +0300

    Create rm.asm

---
+++ src/hexos/16/bootloader/kernel/core/dbc/rm.asm
@@ -0,0 +1,32 @@
+; hexos/src/hexos/16/kernel/core/dbc/rm.asm
+
+rm_cmd db "rm", 0
+
+do_rm:
+    pusha
+    mov si, input_buffer
+    add si, 3
+
+    cmp byte [si], 0
+    je .missing_arg
+    
+    call delete_file
+    jc .success
+    
+    mov si, .error_msg
+    call print_string
+    jmp .done
+    
+.success:
+    jmp .done
+    
+.missing_arg:
+    mov si, .usage_msg
+    call print_string
+    
+.done:
+    popa
+    ret
+    
+.usage_msg db "Usage: rm <filename>", 0x0D, 0x0A, 0
+.error_msg db "Error: Could not delete file", 0x0D, 0x0A, 0
