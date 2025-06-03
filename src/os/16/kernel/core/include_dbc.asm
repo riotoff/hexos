7e81ce6631dd116110627e731740c6535aaeefe1
Arseniy Rayotov <daxkod3243@gmail.com>
2025-06-03 19:24:54 +0300

    Update include_dbc.asm

--- src/os/16/kernel/core/include_dbc.asm
+++ src/os/16/kernel/core/include_dbc.asm
@@ -1,12 +1,12 @@
-; hexos/src/hexos/16/kernel/core/include_dbc.asm
+; hexos/src/os/16/kernel/core/include_dbc.asm
 
-%include "src/hexos/16/kernel/core/dbc/clear.asm"
-%include "src/hexos/16/kernel/core/dbc/exit.asm"
-%include "src/hexos/16/kernel/core/dbc/help.asm"
-%include "src/hexos/16/kernel/core/dbc/hexfetch.asm"
-%include "src/hexos/16/kernel/core/dbc/panic.asm"
-%include "src/hexos/16/kernel/core/dbc/touch.asm"
-%include "src/hexos/16/kernel/core/dbc/ls.asm"
-%include "src/hexos/16/kernel/core/dbc/rm.asm"
-%include "src/hexos/16/kernel/core/dbc/echo.asm"
-%include "src/hexos/16/kernel/core/dbc/cat.asm"
+%include "src/os/16/kernel/core/dbc/clear.asm"
+%include "src/os/16/kernel/core/dbc/exit.asm"
+%include "src/os/16/kernel/core/dbc/help.asm"
+%include "src/os/16/kernel/core/dbc/hexfetch.asm"
+%include "src/os/16/kernel/core/dbc/panic.asm"
+%include "src/os/16/kernel/core/dbc/touch.asm"
+%include "src/os/16/kernel/core/dbc/ls.asm"
+%include "src/os/16/kernel/core/dbc/rm.asm"
+%include "src/os/16/kernel/core/dbc/echo.asm"
+%include "src/os/16/kernel/core/dbc/cat.asm"
